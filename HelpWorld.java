194358499045c9ea073af9a3bac902359aeb2341
Nathan <NSFaltermeier@bluevalleyk12.net>
2016-03-21 13:53:17 -0500

    Work on help

--- HelpWorld.java
+++ HelpWorld.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.awt.Color;
 /**
  * Write a description of class HelpWorld here.
  * 
@@ -16,6 +16,11 @@ public class HelpWorld extends World
     {    
         super(1000, 800, 1);
         setBackground("gray.fw.png");
-        addObject(new Button("back", backTo),29,789); 
+        addObject(new Button("back", backTo),29,789);
+        addObject(new Display("Help",60,Color.LIGHT_GRAY),getWidth() / 2, 25);
+        addObject(new Display("Here you are:",40,Color.LIGHT_GRAY),105,95);
+        addObject(new Display(new GreenfootImage("playerShip.png")), 243,95);
+        addObject(new Display("And here is player 2:",40,Color.LIGHT_GRAY),444,95);
+        addObject(new Display(new GreenfootImage("player2Ship.png")),638,95);
     }
 }
