3e98dc2136393d12b6adb255e28d1fff79994d2c
Nathan <NSFaltermeier@bluevalleyk12.net>
2016-04-11 13:52:08 -0500

    Random mostly works
    As long as you don't cheat
    Also tweaked level 2

--- HubWorld.java
+++ HubWorld.java
@@ -19,8 +19,9 @@ public class HubWorld extends World
     {    
         // Needs a coolio background image
         super(1000, 800, 1);
-        addObject(new Button("world",new SpaceWorld(1,getLevel(Save.level)),new GreenfootImage("\n\nFly solo",50,Color.WHITE,Color.BLACK)),200,200);
-        addObject(new Button("world",new SpaceWorld(2,getLevel(Save.level)),new GreenfootImage("\n\nTeamwork makes the dreamwork",50,Color.WHITE,Color.BLACK)),434,376);
+        Wave[] level = getLevel(Save.level);
+        addObject(new Button("world",new SpaceWorld(1,level),new GreenfootImage("\n\nFly solo",50,Color.WHITE,Color.BLACK)),200,200);
+        addObject(new Button("world",new SpaceWorld(2,level),new GreenfootImage("\n\nTeamwork makes the dreamwork",50,Color.WHITE,Color.BLACK)),434,376);
     }
 
     public Wave[] getLevel(int level)
@@ -63,24 +64,10 @@ public class HubWorld extends World
             {
                 for( int X = 1; X < 11; X++)
                 {
-                    wave1.add(new SpawnableEntity(new EnemyShip1(1),250 + (X * 50),150 + (Y * 50)));
-                }
-            }
-            for( int Y = 1; Y < 3; Y++)
-            {
-                for( int X = 1; X < 11; X++)
-                {
                     wave1.add(new SpawnableEntity(new EnemyShip1(2),250 + (X * 50),50 + (Y * 50)));
                 }
             }
             
-            for( int Y = 1; Y < 3; Y++)
-            {
-                for( int X = 1; X < 11; X++)
-                {
-                    wave2.add(new SpawnableEntity(new EnemyShip2(1),250 + (X * 50),150 + (Y * 50)));
-                }
-            }
             for( int Y = 1; Y < 3; Y++)
             {
                 for( int X = 1; X < 11; X++)
@@ -96,9 +83,9 @@ public class HubWorld extends World
         {
             ArrayList<SpawnableEntity> wave1 = new ArrayList<SpawnableEntity>();
             JOptionPane.showMessageDialog(null,"The next level will be a random level because you have requested a level that hasn't been coded yet.","Game over man, game over.",JOptionPane.WARNING_MESSAGE);
-            String input = JOptionPane.showInputDialog(null,"Enter a seed for the random level or press cancle for a random seed");
+            //String input = JOptionPane.showInputDialog(null,"Enter a seed for the random level or press cancel for a random seed");
             Random random;
-            if(input != null)
+            /**if(input != null)
             {
                 long seed;
                 try
@@ -112,25 +99,27 @@ public class HubWorld extends World
                 random = new Random(seed);
             }
             else
-            {
+            {**/
                 random = new Random();
-            }
+            //}
             
             int times = random.nextInt(31) + 10;
+            if(random.nextInt(1000000) == 0)
+                times = 100;
             for(int i = 0; i < times; i++)
             {
                 int enemyType = random.nextInt(3);
                 if(enemyType == 0)
                 {
-                    wave1.add(new SpawnableEntity(new EnemyShip1(random.nextInt(2) + 1),random.nextInt(711) + 130,random.nextInt(301) + 400));
+                    wave1.add(new SpawnableEntity(new EnemyShip1(random.nextInt(2) + 1),random.nextInt(701) + 140,random.nextInt(301) + 10));
                 }
                 else if(enemyType == 1)
                 {
-                    wave1.add(new SpawnableEntity(new EnemyShip2(random.nextInt(2) + 1),random.nextInt(711) + 130,random.nextInt(301) + 400));
+                    wave1.add(new SpawnableEntity(new EnemyShip2(random.nextInt(2) + 1),random.nextInt(701) + 140,random.nextInt(301) + 10));
                 }
                 else
                 {
-                    wave1.add(new SpawnableEntity(new EnemyShip3(),random.nextInt(711) + 130,random.nextInt(301) + 400));
+                    wave1.add(new SpawnableEntity(new EnemyShip3(),random.nextInt(701) + 140,random.nextInt(301) + 10));
                 }
             }
             
